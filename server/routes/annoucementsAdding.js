e0d74cec28254fe93445e3941645fd846da77906
dev-hemul <dev.hemulll@gmail.com>
2025-05-30 01:02:15 +0300

    Добавил нужные изменения

--- server/routes/annoucementsAdding.js
+++ server/routes/annoucementsAdding.js
@@ -1,21 +1,22 @@
-import express from "express";
+import express from 'express';
 
-import UserModel from "../model/annoucementsModel.js";
+import UserModel from '../model/annoucementsModel.js';
 
 const router = express.Router();
 
-router.post('/annoucementsAdding', async (req, res) => { // Добавлен параметр res
+router.post('/annoucementsAdding', async (req, res) => {
+  // Добавлен параметр res
   const { title, description } = req.body; // Изменили name на title
   console.log(`В БД передано: 
               title: ${title}
               description: ${description}`);
-  
+
   try {
     const doc = new UserModel();
     doc.title = title; // Изменили name на title
     doc.description = description;
     await doc.save();
-    res.status(200).send('Оголошення збережено'); // Добавляем ответ
+    res.status(200).send('Пропозицію збережено'); // Добавляем ответ
   } catch (error) {
     console.error('Помилка збереження:', error);
     res.status(500).send('Помилка сервера');
