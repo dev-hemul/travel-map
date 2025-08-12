5ffb6926385d70f98c8c6c73cb0408ebaf2dac4f
dev-hemul <dev.hemulll@gmail.com>
2025-08-12 08:55:26 +0300

    add AccessToken

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,8 +1,18 @@
 import bcrypt from 'bcrypt';
 import { validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
 import Role from '../model/role.js';
 import User from '../model/users.js';
+
+const generateAccessToken = (userId, roles) => {
+  const payload = {
+    userId,
+    roles,
+  };
+
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h' });
+};
 
 class AuthController {
   async registration(req, res) {
@@ -36,6 +46,13 @@ class AuthController {
       if (!user) {
         return res.status(400).json({ message: `Користувач ${user} не знайдений` });
       }
+      const validPassword = bcrypt.compare(password, user.password);
+      if (!validPassword) {
+        return res.status(400).json({ message: 'Невірний пароль' });
+      }
+
+      const token = generateAccessToken(user._id, user.roles);
+      return res.json({ token });
     } catch (e) {
       console.log(e);
       res.status(400).json({ message: 'Login error' });
