3c4bca78b3658d8d7de002ba8e10f00fa17ae36a
ktrnaaa <rina.pshenichnaya@gmail.com>
2025-10-24 00:22:52 +0200

    fixed cors

--- server/server.js
+++ server/server.js
@@ -33,13 +33,7 @@ app.use((req, res, next) => {
 
 // CORS конфігурація
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || origin === 'http://localhost:5173') {
-      callback(null, 'http://localhost:5173');
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
