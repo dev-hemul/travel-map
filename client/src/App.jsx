fca4e3628c27b7e76a2004b5b3d383f7f3275e77
Evgenij <69868872+dev-hemul@users.noreply.github.com>
2025-11-17 19:20:26 +0200

    TM-133: заблокував відкриття модалки при активації лінійки (#255)
    * TM-133: заблокував відкриття модалки при активації лінійки

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -111,7 +111,7 @@ function App() {
             <>
               <MapView />
               <SupportModalWrapper />
-              <AnnouncementModal />
+              {/*<AnnouncementModal />*/}
             </>
           }
         />
