620aa0a66da714c8e2a4fb41470a5cd5b84cac1d
Stehnii-Andrew <astehnii08@gmail.com>
2025-11-29 21:47:56 +0100

    Додана неробоча заготовка рулетки

--- client/src/components/MapView.jsx
+++ client/src/components/MapView.jsx
@@ -1056,6 +1056,7 @@ const MapView = () => {
         <RouletteWidget
           isMeasureEnabled={isMeasureEnabled}
           onToggleMeasure={() => setIsMeasureEnabled(prev => !prev)}
+        
         />
       </div>
     </div>
