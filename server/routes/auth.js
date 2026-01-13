924b14ba6286fdfacaaf68d655ee33c386855445
ktrnaaa <rina.pshenichnaya@gmail.com>
2026-01-13 17:43:08 +0100

    TM-113: google auth
    TM-113: google auth2
    
    TM-113: google auth + fixed
    
    TM-113: google auth
    
    TM-113: google auth2
    
    TM-113: google auth + fixed
    
    TM-113: google auth
    
    TM-113: google auth
    
    TM-113: google auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,8 +2,10 @@ import express from 'express';
 
 
 import { verifyAccessToken, verifyRefreshToken } from '../middlewares/auth.js';
+import Tokens from '../model/token.js';
 import User from '../model/user.js'; // Імпорт User для GET /profile
-import { register, login, logout, updateProfile, getRefreshToken } from './../controller/authCntrl.js';
+import { register, login, logout, updateProfile, getRefreshToken, createTokens } from './../controller/authCntrl.js';
+import { googleCodeToUser, getGoogleAuthUrl } from './../middlewares/googleAuth.js'
 import {
   validateLoginBody,
   validateRegisterBody,
@@ -18,7 +20,6 @@ router.post('/login', validateLoginBody, (req, res, next) => {
 router.post('/refresh-token', verifyRefreshToken, getRefreshToken);
 router.post('/logout', verifyAccessToken, logout);
 
-// GET /profile для перевірки токена
 router.get('/profile', verifyAccessToken, async (req, res) => {
   try {
     if (!req.userId) {
@@ -34,7 +35,55 @@ router.get('/profile', verifyAccessToken, async (req, res) => {
   }
 });
 
-// POST /profile для оновлення
 router.post('/profile', verifyAccessToken, updateProfile);
+
+router.get('/google/url', getGoogleAuthUrl);
+
+router.post('/google', googleCodeToUser, async (req, res) => {
+  try {
+    const gUser = req.googleUser;
+
+    const emailNorm = (gUser.email || '').trim().toLowerCase();
+
+    let user = await User.findOne({
+      $or: [{ googleId: gUser.googleId }, { email: emailNorm }],
+    });
+
+    if (!user) {
+      user = new User({
+        username: gUser.name || emailNorm.split('@')[0],
+        email: emailNorm,
+        provider: 'google',
+        googleId: gUser.googleId,
+        roles: ['user'],
+      });
+      await user.save();
+    } else {
+      // якщо був local і тепер зайшов через Google
+      if (!user.googleId) user.googleId = gUser.googleId;
+      if (!user.provider) user.provider = 'google';
+      if (!Array.isArray(user.roles) || user.roles.length === 0) user.roles = ['user'];
+      await user.save();
+    }
+
+    await Tokens.deleteMany({ userId: user._id });
+
+    const { accessT, refreshT } = await createTokens(user._id);
+
+    res.cookie('refreshToken', refreshT, {
+      httpOnly: true,
+      secure: false, // локально
+      sameSite: 'lax',
+      maxAge: 21 * 24 * 60 * 60 * 1000,
+      path: '/',
+    });
+
+    return res.json({ accessToken: accessT });
+  } catch (err) {
+    console.error('Google route error:', err);
+    return res.status(500).json({ message: 'Server error' });
+  }
+});
+
 
 export default router;
