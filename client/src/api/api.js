3d7b29a30b2cbd584f09f338981a8ac8993add36
ktrnaaa <rina.pshenichnaya@gmail.com>
2026-03-04 17:43:01 +0100

    TM-200: ban
    TM-200: ban 2.0
    
    TM-200: ban 3.0
    
    TM-200: ban 3.0
    
    TM-200: ban 4.0
    
    TM-200: ban
    
    TM-200: ban 2.0
    
    TM-200: ban 3.0
    
    TM-200: ban 3.0
    
    TM-200: ban 8

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: import.meta.env.VITE_API_URL,
-	withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});  /// це для адмінських роутів, бо перевіряє роль в токені і не пропускає дії по адмінським роутам без цього
 
 export default api;
