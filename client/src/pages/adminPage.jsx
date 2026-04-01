00b63734bfcfde480ceb769f3a046210757ff8fc
dev-hemul <dev.hemulll@gmail.com>
2026-04-01 08:22:06 +0300

    TM-9999: add-test

--- client/src/pages/adminPage.jsx
+++ client/src/pages/adminPage.jsx
@@ -90,7 +90,7 @@ export default function AdminPage() {
 
   return (
     <div className="p-6">
-      <h1 className="mb-4 text-2xl font-semibold">Адміністрування9999</h1>
+      <h1 className="mb-4 text-2xl font-semibold">Адміністрування</h1>
 
       <AdminToolbar
         showUsers={showUsers}
